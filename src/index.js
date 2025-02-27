c46cef9e5bca5133a0a82a0073a2932920387f18
claireingham <claireingham75@gmail.com>
2025-02-27 07:19:59 +0100

    API link added
    API link added. JS not currently working

--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ function generatePoem(event) {
   event.preventDefault();
 
   new Typewriter("#poem", {
-    strings:"God could not be everywhere, so he made mothers",
-    autostart: true;
+    strings: "God could not be everywhere, so he made mothers",
+    autostart: true,
     delay: 1,
     cursor: "",
   });
-  alert("Generating Poem");
 }
+
+let apiKey = "2ef76df8bbtdbob65420d8afef3dc0e3";
+let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
 let poemFormElement = document.querySelector("#poem-generator-form");
 poemFormElement.addEventListener("submit", generatePoem);
