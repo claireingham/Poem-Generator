113b7d1b3cdc3e5c1c4be0364044bcea1af57d22
claireingham <claireingham75@gmail.com>
2025-03-01 06:40:50 +0100

    Links amended
    Links to GitHub and Netlify amended, JS deleted.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function displayPoem(response) {
   new Typewriter("#poem", {
     strings: "God could not be everywhere, so he made mothers",
     autostart: true,
@@ -9,8 +7,17 @@ function generatePoem(event) {
   });
 }
 
-let apiKey = "2ef76df8bbtdbob65420d8afef3dc0e3";
-let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+function generatePoem(event) {
+  event.preventDefault();
+
+  let apiKey = "2ef76df8bbtdbob65420d8afef3dc0e3";
+  let context =
+    "You are a poetry expert and love writing short 4 line poems based on a one word prompt. Provide the output in basic html. Do not exceed four lines";
+  let prompt = "";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayPoem);
+}
 
 let poemFormElement = document.querySelector("#poem-generator-form");
 poemFormElement.addEventListener("submit", generatePoem);
