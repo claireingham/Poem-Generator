8af913df8d598ef986af2527061b9ce24d230424
claireingham <claireingham75@gmail.com>
2025-03-01 06:52:10 +0100

    Add Typewriter
    Add Typewriter and JS event listener

--- src/index.js
+++ src/index.js
@@ -1,22 +1,12 @@
-function displayPoem(response) {
-  new Typewriter("#poem", {
-    strings: "God could not be everywhere, so he made mothers",
-    autostart: true,
-    delay: 1,
-    cursor: "",
-  });
-}
-
 function generatePoem(event) {
   event.preventDefault();
 
-  let apiKey = "2ef76df8bbtdbob65420d8afef3dc0e3";
-  let context =
-    "You are a poetry expert and love writing short 4 line poems based on a one word prompt. Provide the output in basic html. Do not exceed four lines";
-  let prompt = "";
-  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-
-  axios.get(apiUrl).then(displayPoem);
+  new Typewriter("#poem", {
+    strings: "Here is a poem....",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
